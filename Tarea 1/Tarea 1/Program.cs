51293d65b7e8813c992297921a5ac80893952cae
Justin Acuña <justingilberto2000@gmail.com>
2022-08-07 16:10:42 -0600

    Add new project in repo

---
+++ Tarea 1/Tarea 1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tarea_1
+{
+    internal class Program
+    {
+        static int mayorArreglo(int[] arreglo)
+        {
+            int maximo = arreglo[0];                //1
+            int tamaño = arreglo.Length;            //1+n
+            for (int i = 0; i < tamaño; i++)        //2n+2
+            {
+                //Console.WriteLine(arreglo[i]);      
+                if (arreglo[i] > maximo)            //n+1
+                    maximo = arreglo[i];            //+1
+            }
+            return maximo;                          //+1
+        }                                           //1+1+1+1+n+2n+2+n+1 = 4n+7
+
+        static int[] crearArreglo(int[] arreglo)
+        {
+            int tamaño = arreglo.Length;
+            Random aleatorio = new Random();
+
+            for (int i = 0; i < tamaño; i++)
+            {
+                int nRandom = aleatorio.Next(0, 10000);
+                arreglo[i] = nRandom;
+            }
+            return arreglo;
+        }
+        static void Main(string[] args)
+        {
+            int[] arreglo = new int[500];
+            crearArreglo(arreglo);
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            int x = mayorArreglo(arreglo);
+
+            stopWatch.Stop();
+            Console.WriteLine("Tiempo: "+stopWatch.Elapsed.Duration());
+            Console.WriteLine("El máximo es: "+x);
+            
+        }
+    }
+}
